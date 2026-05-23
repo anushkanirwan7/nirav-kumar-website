ac3f088d1312316d9d2145b36594ab1858159bfd
Anushka Nirwan <anushkanirwan7@gmail.com>
2026-05-23 12:11:51 +0530

    Fixed hamburger click fix

--- js/main.js
+++ js/main.js
@@ -47,11 +47,15 @@ filterButtons.forEach(btn => {
   });
 });
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener("DOMContentLoaded", () => {
 
-if (hamburger && navLinks) {
-    hamburger.addEventListener("click", () => {
-        navLinks.classList.toggle("show");
-    });
-}
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    if (hamburger && navLinks) {
+        hamburger.addEventListener("click", () => {
+            navLinks.classList.toggle("show");
+        });
+    }
+
+});
